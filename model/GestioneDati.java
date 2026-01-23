e5e35240c86b03a344db8273bfc5c82225bafe70
Denji411 <daniele.matranga411@gmail.com>
2026-01-23 18:26:24 +0100

    Primo commit

---
+++ model/GestioneDati.java
@@ -0,0 +1,63 @@
+package Osservatorio.model;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public final class GestioneDati {
+    private static LinkedList<Stato> stati = new LinkedList<>();
+    private static LinkedList<Stato> scarti = new LinkedList<>();
+
+    public static void statiPerContinente() {
+        System.out.println("=== Stati Validati Per Continente ===");
+        
+        stati.stream()
+            .collect(Collectors.groupingBy(
+                Stato::getContinente,
+                Collectors.counting()
+            ))
+            .forEach((sta, n) ->
+                System.out.println(sta + ": " + n)
+            );
+        
+        System.out.println();
+    }
+
+    public static void economiaMediaContinente() {
+        System.out.println("=== Media Economia Per Continente ===");
+
+        stati.stream()
+            .collect(Collectors.groupingBy(
+                Stato::getContinente,
+                Collectors.averagingDouble(Stato::getEconomia)
+            ))
+            .entrySet().stream()
+            .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+            .forEach(e ->
+                System.out.println(e.getKey() + ": " + e.getValue())
+            );
+
+        System.out.println();
+    }
+
+    public static void economiaMondiale() {
+        long economiaMondiale = stati.stream()
+            .mapToLong(Stato::getEconomia)
+            .sum();
+
+        System.out.println("=== Economia Mondiale ===");
+        System.out.println(economiaMondiale);
+    }
+
+    public static void topPerContinente() {
+        System.out.println("=== Stati Più Ricchi Per Continente ===");
+
+        stati.stream()
+            .collect(Collectors.groupingBy(
+                Stato::getContinente,
+                Collectors.maxBy(Stato::getEconomia)
+            ))
+            .forEach(e ->
+                System.out.println(e.getKey() + ": " +e.getValue());
+            );
+    }
+}
---
+++ model/Stato.java
@@ -0,0 +1,16 @@
+package Osservatorio.model;
+
+import Osservatorio.resources.Continente;
+
+public class Stato {
+    long economia;
+    Continente continente;
+
+    public long getEconomia() {
+        return economia;
+    }
+
+    public Continente getContinente() {
+        return continente;
+    }
+}
